e6bb4561fb8f149511a2b8e3e53f0b79597097d5
Riaan-jansen <126263953+Riaan-jansen@users.noreply.github.com>
2023-11-29 12:10:38 +0000

    linting

--- test-main/classrooms.py
+++ test-main/classrooms.py
@@ -13,9 +13,9 @@ range = np.arange(0,1000000,1)
 
 for i in range:
     if random.uniform(0,1) <= 0.49:
-        total = total + 1;
+        total = total + 1
     else:
-        total = total - 1;
+        total = total - 1
 i = i + 1
 
 print(total)
@@ -26,14 +26,14 @@ t = 1000
 for i in range:
     if t % 3 == 0:
         if random.uniform(0,1) <= 0.09:
-            t = t + 1;
+            t = t + 1
         else:
-            t = t - 1;
+            t = t - 1
     else:
         if random.uniform(0,1) <= 0.74:
-            t = t + 1;
+            t = t + 1
         else:
-            t = t - 1;
+            t = t - 1
 i = i + 1
 
 print(t)
